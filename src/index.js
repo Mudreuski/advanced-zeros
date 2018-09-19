3f6132b4393f0f42aac8d3d42b89eff844ff744c
Mudreuski <bahus1994@mail.ru>
2018-09-20 00:05:25 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let enumPrime = {};
+  let massSol = [];
+  let cache = base;
+
+  for (let i = 2; i <= cache; i++) {
+    
+    while (cache % i == 0) {
+      
+      if (enumPrime[i]) {
+        enumPrime[i] = enumPrime[i] + 1;
+      }
+      else{
+        enumPrime[i] = 1;
+      }
+
+      cache /= i;
+    }
+    
+  }
+
+  for (const key in enumPrime) {
+    massSol.push( solutions(number, +key, enumPrime[key]) );
+  }
+
+  return Math.min.apply(0, massSol);
+};
+
+function solutions(number, enumPrime, quantity) {
+  let cache = 0;
+
+  while (number > 0) {
+    number = Math.floor(number / enumPrime);
+    cache += number;
+  }
+
+  return Math.floor(cache / quantity);
 }
